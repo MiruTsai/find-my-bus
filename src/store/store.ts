62fcee9599c99326b1c55de62008111d9ce973cf
MiruTsai <miru.tsai@thinkpower-info.com>
2022-04-10 21:44:19 +0800

    set up store

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,11 +1,15 @@
 import { createStore } from 'vuex'
-
+import { busAPI } from '../request/api/busAPI'
+import Station from '../types/station'
+import Route from '@/types/route'
 export default createStore({
   state: {
       position: {
         latitude: 0,
         longitude: 0
-      }    
+      },
+      stations: [] as Station[],
+      routes: [] as Route[]
   },
   mutations: {
     updatePosition (state) {
@@ -13,12 +17,27 @@ export default createStore({
             state.position.latitude = position.coords.latitude;
             state.position.longitude = position.coords.longitude;            
         });
+    },
+    getNearStations (state) {      
+      busAPI.getNearByStations().then((res) => {
+        res.sort((a:Station, b:Station)=> {
+          return a.StationName.Zh_tw.localeCompare(b.StationName.Zh_tw)
+        })
+        state.stations = res;      
+      })
+    },
+    getStopRoutes (state, stopId) {
+      busAPI.getStopRoutes(stopId).then((res) => {
+        state.routes = res;      
+      })
     }
   },
   actions: {
-
-  },
-  getters: {
-
+    getNearStations ({ commit }) {
+      commit('getNearStations')
+    },
+    getStopRoutes ({ commit }, stopId) {
+      commit('getStopRoutes', stopId)
+    }
   }
 })
