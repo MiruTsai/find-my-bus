03db61aa3a34922c514ceabe3ec0843ad75a2c31
MiruTsai <miru.tsai@thinkpower-info.com>
2022-04-24 22:04:44 +0800

    store setting

--- src/App.vue
+++ src/App.vue
@@ -4,21 +4,19 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import { useStore } from "vuex";
+import { defineComponent } from 'vue'
+import { useStore } from "vuex"
 export default defineComponent({
     name: 'App',
     setup () {
-      const store = useStore();
+      const store = useStore()
       store.commit('updatePosition')
+      window.addEventListener('beforeunload',() => sessionStorage.setItem('state', JSON.stringify(store.state)))
     }
 });
 </script>
 
 <style lang="scss">
-#app {  
-  width: 100vw;
-}
 .bg {
   position: fixed;
   background: linear-gradient(180deg, #323232 0.52%, #0A0A0B 100%);
--- src/store/store.ts
+++ src/store/store.ts
@@ -4,51 +4,124 @@ import Station from '../types/station'
 import Route from '@/types/route'
 import Estimated from '@/types/estimatedTime'
 export default createStore({
-  state: {
-      position: {
-        latitude: 0,
-        longitude: 0
-      },
-      stations: [] as Station[],
-      routes: [] as Route[],
-      routeEstimateTime: [] as Estimated[],
-      stationId: '' as String
+  state: sessionStorage.getItem('state') ? JSON.parse(sessionStorage.getItem('state') || '{}') : {
+    position: {
+      latitude: 0,
+      longitude: 0
+    },
+    city: 'Taipei' as string | null | undefined,
+    stations: [] as Station[],
+    routes: [] as Route[],
+    routeEstimateTime: [] as Estimated[],
+    stationId: '' as string
   },
   mutations: {
-    updatePosition (state) {
-        navigator.geolocation.getCurrentPosition((position) => {
-            state.position.latitude = position.coords.latitude;
-            state.position.longitude = position.coords.longitude;            
-        });
+    updatePosition(state) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        state.position.latitude = position.coords.latitude
+        state.position.longitude = position.coords.longitude
+      });
     },
-    getNearStations (state) {      
+    updateRoutes(state, routes) {
+      state.routes = routes
+    },
+    updateCity(state, city) {
+      state.city = city
+    },
+    getNearStations(state) {
+      fetch(`https://api.nlsc.gov.tw/other/TownVillagePointQuery/${state.position.longitude}/${state.position.latitude}`)
+        .then(res => res.text())
+        .then(str => {
+          const dom: Document = new window.DOMParser().parseFromString(str, "text/xml")
+          const city: HTMLElement | null = dom.querySelector('ctyName')
+          state.city = converCityName(city?.textContent)
+        })
       busAPI.getNearByStations().then((res) => {
-        res.sort((a:Station, b:Station)=> {
+        res.sort((a: Station, b: Station) => {
           return a.StationName.Zh_tw.localeCompare(b.StationName.Zh_tw)
         })
-        state.stations = res;      
+        state.stations = res;
       })
     },
-    getStopRoutes (state, stationId) {
+    getStopRoutes(state, stationId: string) {
       busAPI.getStopRoutes(stationId).then((res) => {
-        state.routes = res;      
+        state.routes = res
       })
     },
-    getRouteEstimateTime (state, routeName) {
-      busAPI.getRouteEstimatedTime(routeName).then((res) => {
-        state.routeEstimateTime = res;
+    getRouteEstimateTime(state, routeName: string) {
+      busAPI.getRouteEstimatedTime(routeName).then(res => state.routeEstimateTime = res)
+    },
+    searchRoute(state, keyword: string) {
+      busAPI.searchRoute(keyword).then((res) => {
+        state.routes = res
       })
     }
   },
   actions: {
-    getNearStations ({ commit }) {
+    getNearStations({ commit }) {
       commit('getNearStations')
     },
-    getStopRoutes ({ commit }, stationId) {
+    getStopRoutes({ commit }, stationId) {
       commit('getStopRoutes', stationId)
     },
     getRouteEstimateTime({ commit }, routeName) {
       commit('getRouteEstimateTime', routeName)
+    },
+    searchRoute({ commit }, keyword) {
+      commit('searchRoute', keyword)
+    },
+    updateRoutes({ commit }, routes) {
+      commit('updateRoutes', routes)
+    },
+    updateCity({ commit }, city) {
+      commit('updateCity', city)
     }
   }
 })
+
+const converCityName = (text) => {
+  switch (text) {
+    case '台北市':
+      return 'Taipei'
+    case '新北市':
+      return 'NewTaipei'
+    case '桃園市':
+      return 'Taoyuan'
+    case '台中市':
+      return 'Taichung'
+    case '台南市':
+      return 'Tainan'
+    case '基隆市':
+      return 'Keelung'
+    case '新竹市':
+      return 'Hsinchu'
+    case '新竹縣':
+      return 'HsinchuCounty'
+    case '苗栗縣':
+      return 'MiaoliCounty'
+    case '彰化縣':
+      return 'ChanghuaCounty'
+    case '南投縣':
+      return 'NantouCounty'
+    case '雲林縣':
+      return 'YunlinCounty'
+    case '嘉義縣':
+      return 'ChiayiCounty'
+    case '嘉義市':
+      return 'Chiayi'
+    case '屏東縣':
+      return 'PingtungCounty'
+    case '宜蘭縣':
+      return 'YilanCounty'
+    case '花蓮縣':
+      return 'HualienCounty'
+    case '台東縣':
+      return 'TaitungCounty'
+    case '金門縣':
+      return 'KinmenCounty'
+    case '澎湖縣':
+      return 'PenghuCounty'
+    case '連江縣':
+      return 'LienchiangCounty'
+  }
+}
